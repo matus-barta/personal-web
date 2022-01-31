7dda68ea188777f141910c5a2b94bd0a993c30b4
Matus Barta <matus.barta64@gmail.com>
2022-01-31 19:28:21 +0100

    fix images

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,7 +1,7 @@
 <nav class="bg-gray-500 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
   <div class="max-w-5xl mx-auto px-4">
     <div class="flex items-center justify-between h-16">
-      <img src="../static/A.png" class="w-12 h-12 rounded-xl" alt="logo"/>
+      <img src="%svelte.assets%/A.png" class="w-12 h-12 rounded-xl" alt="logo"/>
       <div class="flex space-x-4 text-white">
         <a href="/">About</a>
         <a href="/">Projects</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@ import Card from "../components/card.svelte";
 
 const card = {
     title: "Web is under construction",
-    img: "../static/information.png",
+    img: "%svelte.assets%/information.png",
     img_alt: "information",
     text: "We are working on it"
 };
