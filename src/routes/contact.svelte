3284569da7876ef02ab994602a2c07a8c483ee66
Matus Barta <matus.barta64@gmail.com>
2022-02-19 00:55:33 +0100

    implement success page with timed redirect

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -18,7 +18,7 @@
 
 <h1>Feel free to contact me</h1>
 
-<form name="contact-form" method="post" data-netlify-recaptcha="true" data-netlify="true" class="bg-gray-700 rounded-xl shadow-md px-8 pt-6 pb-6 flex justify-center">
+<form name="contact-form" method="post" data-netlify-recaptcha="true" data-netlify="true" action="/success" class="bg-gray-700 rounded-xl shadow-md px-8 pt-6 pb-6 flex justify-center">
     <div class="w-full md:w-3/4">
         <input type="hidden" name="form-name" value="contact-form" />
         <p>
@@ -32,7 +32,7 @@
         </p>
         <div data-netlify-recaptcha="true"></div>
         <p>
-            <button class="px-6 py-2 text-base bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 focus:outline-none rounded-full" type="submit">Send</button>
+            <button class="bg-emerald-500" type="submit">Send</button>
         </p>
     </div>
 </form>
---
+++ src/routes/success.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+    <title>Anonymus09 - Success</title>
+</svelte:head>
+
+<script>
+    import {onMount} from "svelte";
+
+    onMount(()=>{
+        setTimeout(()=>{
+            goHome();
+        },5000)
+    });
+
+    function goHome(){
+        window.location.href="/";
+    }
+</script>
+
+<div class="bg-gray-700 rounded-xl shadow-md px-8 pt-6 pb-6 text-center">
+    <p class="pb-0">Form was successfully sent!
+    <p class="pb-4">Will redirect you in few seconds...</p>
+    <button on:click={goHome}>Go Home!</button>
+</div>
