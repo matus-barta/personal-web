4dbe5e1fe005b87633841442406df9dd268ea022
Matus Barta <matus.barta64@gmail.com>
2022-02-18 23:59:48 +0100

    remove honeypot add recapcha

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -18,10 +18,9 @@
 
 <h1>Feel free to contact me</h1>
 
-<form name="contact-form" method="post" netlify netlify-honeypot="bot-field" class="bg-gray-700 rounded-xl shadow-md px-8 pt-6 pb-6 flex justify-center">
+<form name="contact-form" method="post" netlify data-netlify-recaptcha="true" class="bg-gray-700 rounded-xl shadow-md px-8 pt-6 pb-6 flex justify-center">
     <div class="w-full md:w-3/4">
         <input type="hidden" name="form-name" value="contact-form" />
-        <input type="text" name="bot-field" />
         <p>
             <label>Your Name: <input type="text" name="name" /></label>
         </p>
@@ -31,6 +30,7 @@
         <p>
             <label>Message: <textarea name="message" /></label>
         </p>
+        <div data-netlify-recaptcha="true"></div>
         <p>
             <button class="px-6 py-2 text-base bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 focus:outline-none rounded-full" type="submit">Send</button>
         </p>
