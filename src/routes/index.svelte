63323f10ffa72ffa26ad8e2431e9eeb0bdd236b7
Matus Barta <matus.barta64@gmail.com>
2022-02-19 02:38:50 +0100

    start work on main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,31 @@
-
 <script>
-import Card from "../components/card.svelte";
-
-
-const card = {
-    title: "Web is under construction",
-    img: "./information.png",
-    img_alt: "information",
-    text: "We are working on it"
-};
 
 </script>
 <svelte:head>
     <title>Anonymus09</title>
 </svelte:head>
 
-<div class="flex justify-center">
-    <Card card={card}></Card>
+<div class="bg-gray-700 rounded-xl shadow-md px-3 md:px-6 pt-6 pb-6 flex flex-col">
+    <div class="flex justify-between">
+        <div class="flex flex-col">
+        <h1 class="text-2xl md:text-4xl font-bold mb-1 text-white tracking-tight">
+            Matúš
+            <span class="relative ml-2 inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-sky-500">
+                <span class="relative skew-y-3">
+                     <a class="hover:text-white hover:underline" href=https://twitter.com/anonymus64>@anonymus64</a>
+                </span>                
+            </span>            
+            Barta
+        </h1>
+        <p>IT support, hobby developer, homelab enthusiast, gamer, space nerd.</p>
+        <a class="text-gray-500 hover:underline mt-2" href="/about">More on About page</a>
+        </div>
+        <img class="w-32 h-32 ml-2 rounded-full md:w-40 md:h-40" src="./A.svg" alt="logo">
+    </div>
+    <div class="pt-6 pb-6">
+        <h1 class="text-2xl md:text-4xl font-bold mb-1 text-white tracking-tight">Latest Blog Posts</h1>
+        <p>Well there is none... for now</p>
+        <a class="text-gray-500 hover:underline mt-2" href="/blog">See All Posts</a>
+    </div>
 </div>
+
