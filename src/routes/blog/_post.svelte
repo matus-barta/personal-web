2a63aba1281c386cdd8d22050d6fa26d6fcacbae
Matus Barta <matus.barta64@gmail.com>
2022-02-22 18:53:54 +0100

    update fromating for blog posts

--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -9,6 +9,10 @@
 
 <div class="bg-gray-700 rounded-xl shadow-md px-3 md:px-6 pt-6 pb-6 flex flex-col">
     <h1>{title}</h1>
-    <p>Published: {date}</p>
-    <slot />
+    <div class="flex justify-between">
+        <h3>Matúš</h3>
+        <h3>{date}</h3>
+    </div>
+    <div class="h-1 mb-10 mt-3 bg-gradient-to-r from-emerald-500 via-sky-500 to-blue-700 w-full rounded-xl"></div>
+    <slot/>
 </div>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -4,12 +4,11 @@
 
 <script lang="ts" context="module">
     export const load = async ({ fetch }) => {
-        const posts = await fetch('/api/posts.json')
-        const allPosts = await posts.json()
-
+        const posts = await fetch('/api/posts.json');
+        const allPosts = await posts.json();
         return {
             props: {
-            posts: allPosts
+                posts: allPosts
             }
         }
     }
@@ -19,17 +18,24 @@
     export var posts;
 </script>
 
-<div class="bg-gray-700 rounded-xl shadow-md px-3 md:px-6 pt-6 pb-6 flex flex-col">
-    <ul>
+<h1>Personal Blog</h1>
+<h3>List of all blog posts.</h3>
+
+<div class="bg-gray-700 rounded-xl shadow-md px-3 md:px-6 md:py-6 py-3 flex flex-col mt-5">
+    <div class="flex flex-col space-y-5">
         {#each posts as post}
-        <li>
-        <h2>
-            <a href={post.path}>
-            {post.meta.title}
-            </a>
-        </h2>
-            Published {post.meta.date}
-        </li>
+         <div class="flex">
+            <img src="{post.meta.img}" alt="blog post logo" class="object-cover w-48 h-28 md:w-96 md:h-56 rounded-xl mr-5 md:mr-10"/>
+            <div class="flex flex-col justify-between py-1 rounded-xl w-full">
+                <div class="flex flex-col">
+                    <h2 class="mb-5">
+                        <a href={post.path}>{post.meta.title}</a>
+                    </h2>
+                    <h4>{post.meta.excerpt}</h4>
+                </div>
+                <h3>{post.meta.date}</h3>
+            </div>
+        </div>
         {/each}
-    </ul>
+    </div>
 </div>
