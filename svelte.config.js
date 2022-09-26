a2b1be17f7fa47c1225ba9b3f90dc9d7bf79b778
Matus Barta <matus.barta64@gmail.com>
2022-09-26 22:21:47 +0000

    trying to use latest template but leave old changes

--- svelte.config.js
+++ svelte.config.js
@@ -6,16 +6,6 @@ import { mdsvex } from 'mdsvex';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	kit: {
-		adapter: adapter(),
-		// hydrate the <div id="svelte"> element in src/app.html
-		prerender: {
-            crawl: true,
-            enabled: true,
-            onError: "continue",
-            entries: ['*'],
-        },
-	},
 	extensions: ['.svelte', '.md'],
 	preprocess: [
 		preprocess(),
@@ -25,7 +15,26 @@ const config = {
 			blog: 'src/routes/blog/_post.svelte'
 		}
 		})
-	]
+	],
+	kit: {
+		adapter: adapter({
+		// if true, will create a Netlify Edge Function rather
+		// than using standard Node-based functions
+		edge: false,
+
+		// if true, will split your app into multiple functions
+		// instead of creating a single one for the entire app.
+		// if `edge` is true, this option cannot be used
+		split: false
+		}),
+		// hydrate the <div id="svelte"> element in src/app.html
+		prerender: {
+            crawl: true,
+            enabled: true,
+            onError: "continue",
+            entries: ['*'],
+        },
+	}
 };
 
 export default config;
