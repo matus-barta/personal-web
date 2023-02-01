c231d04e5dfc603d1944163ab8be47513f7363e1
matus-barta <matus.barta64@gmail.com>
2023-02-01 14:46:55 +0100

    Update svelte to 1.0 and some minor changes (#18)
    * update tag in blog
    * add just a stupid newline
    * fix tagged list wast showing tag and missign margin
    * styling for img and code tags - WIP
    * update packages
    * formatting updates

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,35 +1,43 @@
-<svelte:head>
-    <title>Blog - Matus Barta | Anonymus09</title>
-</svelte:head>
-
 <script lang="ts">
-    import type { PageData } from "./$types"
-	
-    export let data: PageData;
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
+
+<svelte:head>
+	<title>Blog - Matus Barta | Anonymus09</title>
+</svelte:head>
 
 <h1>Personal Blog</h1>
 <h3>Here are all the blog posts</h3>
 
 <div class="bg-gray-700 rounded-xl shadow-md px-3 md:px-6 md:py-6 py-3 flex flex-col mt-5">
-    <div class="flex flex-col space-y-10">
-        {#each data.posts as post}
-         <div class="flex">
-            {#if post.meta.img_transparent}
-                <img src="{post.meta.img}" alt="blog post logo" class="object-cover p-3 w-48 md:w-96 rounded-xl mr-4 md:mr-8 bg-slate-100"/>
-            {:else}
-                <img src="{post.meta.img}" alt="blog post logo" class="object-cover w-48 md:w-96 rounded-xl mr-4 md:mr-8"/>
-            {/if}
-            <div class="flex flex-col justify-between py-1 rounded-xl w-full">
-                <div class="flex flex-col">
-                    <h2 class="mb-5">
-                        <a href={post.path}>{post.meta.title}</a>
-                    </h2>
-                    <h4>{post.meta.excerpt}</h4>
-                </div>
-                <h3>{post.meta.date}</h3>
-            </div>
-        </div>
-        {/each}
-    </div>
+	<div class="flex flex-col space-y-10">
+		{#each data.posts as post}
+			<div class="flex">
+				{#if post.meta.img_transparent}
+					<img
+						src={post.meta.img}
+						alt="blog post logo"
+						class="object-cover p-3 w-48 md:w-96 rounded-xl mr-4 md:mr-8 bg-slate-100"
+					/>
+				{:else}
+					<img
+						src={post.meta.img}
+						alt="blog post logo"
+						class="object-cover w-48 md:w-96 rounded-xl mr-4 md:mr-8"
+					/>
+				{/if}
+				<div class="flex flex-col justify-between py-1 rounded-xl w-full">
+					<div class="flex flex-col">
+						<h2 class="mb-5">
+							<a href={post.path}>{post.meta.title}</a>
+						</h2>
+						<h4>{post.meta.excerpt}</h4>
+					</div>
+					<h3>{post.meta.date}</h3>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,28 +1,47 @@
 <script lang="ts">
-    //import {} from "../../../../static/prism/prism"
-    import type { PageData } from "./$types"
-    export let data: PageData;
+	//import {} from "../../../../static/prism/prism"
+	import type { PageData } from './$types';
+	export let data: PageData;
 </script>
 
 <svelte:head>
-    <title>{data.meta.title}</title>
-    <link href="/prism/prism.css" rel="stylesheet" />
+	<title>{data.meta.title}</title>
+	<link href="/prism/prism.css" rel="stylesheet" />
 </svelte:head>
 
 <div class="bg-gray-700 rounded-xl flex flex-col">
-    {#if data.meta.img_transparent}
-        <img src="{data.meta.img}" alt="blog title" class="w-full h-44 rounded-xl rounded-b-none object-cover bg-slate-100 px-3"/>
-    {:else}
-        <img src="{data.meta.img}" alt="blog title" class="w-full h-44 rounded-xl rounded-b-none object-cover"/>
-    {/if}
-    <div class="px-3 md:px-6 pt-2 flex flex-col">      
-        <h1 class="p-0 mt-1 mb-5">{data.meta.title}</h1>
-        <h3 class="w-full text-right align-text-bottom">{data.meta.date}</h3>
-        <div class="h-1 my-1 bg-gradient-to-r from-emerald-500 via-sky-500 to-blue-700 w-full rounded-xl"/>
-        <div class="my-5">
-            <div>
-                {@html data.html}
-            </div>
-        </div>
-    </div>
+	{#if data.meta.img_transparent}
+		<img
+			src={data.meta.img}
+			alt="blog title"
+			class="w-full h-44 rounded-xl rounded-b-none object-cover bg-slate-100 px-3"
+		/>
+	{:else}
+		<img
+			src={data.meta.img}
+			alt="blog title"
+			class="w-full h-44 rounded-xl rounded-b-none object-cover"
+		/>
+	{/if}
+	<div class="px-3 md:px-6 pt-2 flex flex-col">
+		<h1 class="p-0 mt-1 mb-5">{data.meta.title}</h1>
+		<h3 class="w-full text-right align-text-bottom">{data.meta.date}</h3>
+		<div
+			class="h-1 my-1 bg-gradient-to-r from-emerald-500 via-sky-500 to-blue-700 w-full rounded-xl"
+		/>
+		<div class="my-5">
+			<div>
+				{@html data.html}
+			</div>
+		</div>
+	</div>
 </div>
+
+<style>
+	img {
+		@apply rounded-xl;
+	}
+	code {
+		@apply text-rose-300;
+	}
+</style>
