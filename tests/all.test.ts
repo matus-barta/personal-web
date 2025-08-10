1875fadbd8cbf3a1764ff2a71d6ec3f437454836
Matus Barta <matus.barta64@gmail.com>
2025-08-11 00:38:12 +0200

    fix the "temp" tests

--- tests/all.test.ts
+++ tests/all.test.ts
@@ -1,22 +1,25 @@
 import type { Post } from '$lib/types';
 import { expect, test } from '@playwright/test';
 
+const BLOGPOSTS_COUNT = 5;
+const PROJECTS_COUNT = 5;
+
 test('check content of index', async ({ page }) => {
 	await page.goto('/');
 	await expect(
-		page.getByText(`IT guy, hobby developer, homelab enthusiast, gamer, space nerd.`)
+		page.getByText(`IT professional, hobby developer, homelab enthusiast, gamer, space nerd.`)
 	).toBeVisible();
-	await expect(page.getByTestId(`latest_blog_post`)).toHaveCount(5);
+	await expect(page.getByTestId(`latest_blog_post`)).toHaveCount(5); //Always 5
 });
 
 test('check content of blog', async ({ page }) => {
 	await page.goto('/blog');
-	await expect(page.getByTestId(`blog_post`)).toHaveCount(5);
+	await expect(page.getByTestId(`blog_post`)).toHaveCount(BLOGPOSTS_COUNT);
 });
 
 test('check content of projects', async ({ page }) => {
 	await page.goto('/projects');
-	await expect(page.getByTestId(`post`)).toHaveCount(5);
+	await expect(page.getByTestId(`post`)).toHaveCount(PROJECTS_COUNT);
 });
 
 test('check content of blogpost open-rmm', async ({ page }) => {
@@ -28,5 +31,5 @@ test('Check API', async ({ request }) => {
 	const posts = await request.get(`/api/posts`);
 	expect(posts.ok()).toBeTruthy();
 	const data: Post[] = await posts.json();
-	expect(data[0].slug == 'open-rmm').toBeTruthy();
+	expect(data[BLOGPOSTS_COUNT - 1].slug == 'best-way-to-manage-nodejs').toBeTruthy();
 });
