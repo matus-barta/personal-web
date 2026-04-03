d74b4408e57bbb57a83ae4a680ae15bc5b057a96
Matus Barta <matus.barta64@gmail.com>
2026-04-03 14:48:36 +0200

    pnpm format

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
 			// instead of creating a single one for the entire app.
 			// if `edge` is true, this option cannot be used
 			split: false
-		}),		// hydrate the <div id="svelte"> element in src/app.html
+		}), // hydrate the <div id="svelte"> element in src/app.html
 		prerender: {
 			crawl: true,
 			entries: ['*']
